c85ae4dd3abfae93d6bcd7091d73d362cc7f3380
Owais Tahir <oo_wais@hotmail.com>
2023-06-05 10:10:05 +0500

    completed prediction pipeline

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.exception import CustomException
 from src.logger import logging
 import os,sys
 from src.pipeline.train_pipeline import TrainingPipeline
+from src.pipeline.predict_pipeline import PredictionPipeline
 
 app = Flask(__name__)
 
@@ -21,6 +22,25 @@ def train_route():
     except Exception as e:
         raise CustomException(e,sys)
     
+@app.route("/predict", methods = ['POST','GET'])
+def upload():
+    try:
+        if request.method == 'POST':
+            prediction_pipeline = PredictionPipeline(request)
+            
+            prediction_file_detail = prediction_pipeline.run_pipeline()
+            
+            logging.info("Prediction completed. Downloading prediction file")
+            
+            return send_file(prediction_file_detail.prediction_file_path, 
+                             download_name=prediction_file_detail.prediction_file_name,
+                             as_attachment=True)
+            
+        else:
+            return render_template('upload_file.html')
+        
+    except Exception as e:
+        raise CustomException(e,sys)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
